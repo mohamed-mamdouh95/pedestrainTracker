fad3d5c95bc7801be21295fe9ac506135c18ff2e
mohamed-mamdouh95 <mm95mido@gmail.com>
2017-05-01 18:31:57 +0200

    multiple person tracking

--- pedestrianTracker.py
+++ pedestrianTracker.py
@@ -1,6 +1,7 @@
 import dlib
 from darkflow.net.build import TFNet
 import cv2
+import time
 
 options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.1}
 tfnet = TFNet(options)
@@ -8,29 +9,36 @@ it= 1
 img = cv2.imread("./test/"+str(it)+".jpg")
 result = tfnet.return_predict(img)
 points = []
-points.append(( result[1]['topleft']['x'],result[1]['topleft']['y'],result[1]['bottomright']['x']
-			,result[1]['bottomright']['y']))
+for i in range (0,len(result)):
+    if result[i]['label'] == 'person' :
+        points.append(( result[i]['topleft']['x'],result[i]['topleft']['y'],result[i]['bottomright']['x']
+			,result[i]['bottomright']['y']))
 print points
 if not points:
     print "ERROR: No object to be tracked."
     exit()
-tracker = dlib.correlation_tracker()
-tracker.start_track(img, dlib.rectangle(*points[0]))
+tracker = [dlib.correlation_tracker() for _ in xrange(len(points))]
+[tracker[i].start_track(img, dlib.rectangle(*rect)) for i, rect in enumerate(points)]
 
 while it < 112:
    
     img = cv2.imread("./test/"+str(it)+".jpg")
-    tracker.update(img)
-    rect = tracker.get_position()
-    pt1 = (int(rect.left()), int(rect.top()))
-    pt2 = (int(rect.right()), int(rect.bottom()))
-    cv2.rectangle(img, pt1, pt2, (255, 255, 255), 3)
-    print "Object tracked at [{}, {}] \r".format(pt1, pt2),
+    x = 1
+    for i in xrange(len(tracker)):
+            tracker[i].update(img)
+            # Get the position of th object, draw a 
+            # bounding box around it and display it.
+            rect = tracker[i].get_position()
+            pt1 = (int(rect.left()), int(rect.top()))
+            pt2 = (int(rect.right()), int(rect.bottom()))
+            cv2.rectangle(img, pt1, pt2, (255, 255, 255), 3)
+            cv2.putText(img,str(x),pt1,cv2.FONT_HERSHEY_SIMPLEX, 2,(255,255,255),2)
+            x = x+1 
     cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-    print it
     cv2.imshow("Image", img)
     if cv2.waitKey(1) == 27:
         break
+    time.sleep(0.2)
     it = it +2
 cv2.destroyAllWindows()
 
